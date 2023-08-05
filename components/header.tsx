a712fa9e9657dc463318c06533b4680cc88b9797
SGUF1 <skaranpreetbs@gmail.com>
2023-08-05 03:07:51 +0200

    app

--- components/header.tsx
+++ components/header.tsx
@@ -3,19 +3,24 @@
 import React from 'react'
 import Image from 'next/image'
 import SearchBar from './searchbar'
-import {useRouter} from 'next/navigation'
+import { useRouter } from 'next/navigation'
 
 const Header = () => {
     const router = useRouter()
-    
+    const preventDefault = (event: any) => {
+        event.preventDefault();
+    };
     return (
         <div className='flex justify-between items-center p-2 lg:p-5 lg:px-20 w-full '>
-            <div>
+            <div onDragStart={preventDefault}
+                onContextMenu={preventDefault}
+                // @ts-ignore
+                style={{ userDrag: 'none', userSelect: 'none' }}>
                 {/* <Image src={"https://res.cloudinary.com/dg2hpjtdh/image/upload/v1688768031/htzdr7jksuvpdn3uy3vc.jpg"} onClick={() => router.push("/")} alt='logo' width={50} height={50} /> */}
-                <Image src={"/ds_logo.svg"} alt='image' width={140} height={90} className='w-[150px] lg:w-[200px]' onClick={() => router.push("/")}/>
+                <Image src={"/dxs_logo.png"} alt='image' width={140} height={90} className='w-[150px] lg:w-[200px]' onClick={() => router.push("/")} />
             </div>
             <div>
-                <SearchBar/>
+                <SearchBar />
             </div>
 
         </div>
--- components/leftbar.tsx
+++ components/leftbar.tsx
@@ -11,32 +11,32 @@ const LeftBar = () => {
   const pathname = usePathname()
   const router = useRouter()
   return (
-    <div className={cn("lg:-mt-10 lg:-ml-14 mt-4 border-r relative flex flex-col gap-6 h-full justify-center sm:items-center border-red-200 sm:w-48 lg:w-44", isOpen ? "w-full" : "w-[50px]")}>
-      <div className={cn("flex items-center cursor-pointer ", pathname === "/" && "text-red-200")} onClick={() => { router.push("/"); }}>
+    <div className={cn("lg:-mt-10 lg:-ml-14 mt-4 border-r relative flex flex-col gap-6 h-full justify-center sm:items-center border-red-500 sm:w-48 lg:w-44", isOpen ? "w-full" : "w-[50px]")}>
+      <div className={cn("flex items-center cursor-pointer ", pathname === "/" && "text-red-500")} onClick={() => { router.push("/"); }}>
         <Home className="h-7 w-7 " />
         <span className="ml-2 hidden sm:block">Home</span>
       </div>
-      <div className={cn("flex items-center cursor-pointer ", pathname === "/eventi" && "text-red-200")} onClick={() => { router.push("/eventi"); }}>
+      <div className={cn("flex items-center cursor-pointer ", pathname === "/eventi" && "text-red-500")} onClick={() => { router.push("/eventi"); }}>
         <PartyPopper className="h-7 w-7" />
         <span className="ml-2 hidden sm:block" >Eventi</span>
       </div>
-      <div className={cn("flex items-center cursor-pointer ", pathname === "/like-rank" && "text-red-200")} onClick={() => { router.push("/like-rank"); }}>
+      <div className={cn("flex items-center cursor-pointer ", pathname === "/like-rank" && "text-red-500")} onClick={() => { router.push("/like-rank"); }}>
         <Trophy className="h-7 w-7" />
         <span className="ml-2 hidden sm:block">Classifica</span>
       </div>
-      <div className={cn("flex items-center cursor-pointer ", pathname === "/liste" && "text-red-200")} onClick={() => { router.push("/liste"); }}>
+      <div className={cn("flex items-center cursor-pointer ", pathname === "/liste" && "text-red-500")} onClick={() => { router.push("/liste"); }}>
         <List className="h-7 w-7" />
         <span className="ml-2 hidden sm:block">Liste</span>
       </div>
-      <div className={cn("flex items-center cursor-pointer ", pathname === "/biglietti" && "text-red-200")} onClick={() => { router.push("/biglietti"); }}>
+      <div className={cn("flex items-center cursor-pointer ", pathname === "/biglietti" && "text-red-500")} onClick={() => { router.push("/biglietti"); }}>
         <Ticket className="h-7 w-7" />
         <span className="ml-2 hidden sm:block">Biglietti</span>
       </div>
-      <div className={cn("flex items-center cursor-pointer ", pathname === "/prenotati" && "text-red-200")} onClick={() => { router.push("/prenotati"); }}>
+      <div className={cn("flex items-center cursor-pointer ", pathname === "/prenotati" && "text-red-500")} onClick={() => { router.push("/prenotati"); }}>
         <Calendar className="h-7 w-7" />
         <span className="ml-2 hidden sm:block">Tavoli</span>
       </div>
-      <div className={cn("flex items-center cursor-pointer ", pathname === "/preferiti" && "text-red-200")} onClick={() => { router.push("/preferiti"); }}>
+      <div className={cn("flex items-center cursor-pointer ", pathname === "/preferiti" && "text-red-500")} onClick={() => { router.push("/preferiti"); }}>
         <Heart className="h-7 w-7" />
         <span className="ml-2 hidden sm:block">Preferiti</span>
       </div>
